424126b5215171be706a136a69060ec31c86ef4d
Maxim Shevtsov <68514853+Salivanch@users.noreply.github.com>
2021-06-16 00:55:42 +0300

    Update models.py

--- web/api/fluss_streams/models.py
+++ web/api/fluss_streams/models.py
@@ -7,6 +7,8 @@ from fluss_servers.models import Servers
 from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 
+from .validators import validate_archive_server
+
 
 class Streams(models.Model):
     st = [("0",'Выключен'),("1",'Включен')]
@@ -15,7 +17,7 @@ class Streams(models.Model):
     sourse = models.CharField(verbose_name="Поток на канал", max_length=120)
     fluss_pipelines = models.ForeignKey(Pipelines, verbose_name="Пакет серверов", on_delete=models.CASCADE)
     data_create = models.DateTimeField(verbose_name="Дата создание стрима", default=now)
-    servers_archive = models.ForeignKey(Servers, verbose_name="Список серверов", blank=True, null=True, on_delete=models.CASCADE)
+    servers_archive = models.ForeignKey(Servers, verbose_name="Список серверов", blank=True, null=True, on_delete=models.CASCADE, validators= [validate_archive_server])
     status = models.CharField(verbose_name="Статус", max_length=1, choices=st, default="1")
 
     class Meta:
