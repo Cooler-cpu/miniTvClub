3f7fe358ded38c19b5d8cbffcd530be856720324
Maxim Shevtsov <68514853+Salivanch@users.noreply.github.com>
2021-06-17 17:48:28 +0300

    Доработка стримов

--- web/api/fluss_streams/models.py
+++ web/api/fluss_streams/models.py
@@ -7,7 +7,7 @@ from fluss_servers.models import Servers
 from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 
-from .validators import validate_archive_server
+from .validators import validate_archive_server, validate_piplenes
 
 
 class Streams(models.Model):
@@ -15,9 +15,9 @@ class Streams(models.Model):
 
     name = models.SlugField(verbose_name="Название стрима")
     sourse = models.CharField(verbose_name="Поток на канал", max_length=120)
-    fluss_pipelines = models.ForeignKey(Pipelines, verbose_name="Пакет серверов", on_delete=models.CASCADE)
+    fluss_pipelines = models.ForeignKey(Pipelines, verbose_name="Пакет серверов", on_delete=models.CASCADE, validators= [validate_piplenes])
     data_create = models.DateTimeField(verbose_name="Дата создание стрима", default=now)
-    servers_archive = models.ForeignKey(Servers, verbose_name="Список серверов", blank=True, null=True, on_delete=models.CASCADE, validators= [validate_archive_server])
+    servers_archive = models.ForeignKey(Servers, verbose_name="Архив на сервере", blank=True, null=True, on_delete=models.SET_NULL, validators= [validate_archive_server])
     status = models.CharField(verbose_name="Статус", max_length=1, choices=st, default="1")
 
     class Meta:
@@ -32,9 +32,9 @@ class Streams(models.Model):
         sr = StreamRequest(self)
         sr.update_stream()
 
-    # def delete(self):
-    #     super(Streams, self).delete()
-    #     sr = StreamRequest(self)
-    #     sr.delete_stream()
+    def delete(self):
+        super(Streams, self).delete()
+        sr = StreamRequest(self)
+        sr.delete_stream()
 
 
