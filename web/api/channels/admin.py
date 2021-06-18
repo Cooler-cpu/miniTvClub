8b5fde5dcfd09e9c83efb31d450bf13699790c1f
Maxim Shevtsov <68514853+Salivanch@users.noreply.github.com>
2021-06-18 16:03:49 +0300

    Сортировка в админке для каналов

--- web/api/channels/admin.py
+++ web/api/channels/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Channels, Epg
 
-admin.site.register(Channels)
+from adminsortable2.admin import SortableAdminMixin
+
+
+class ChannelsAdmin(SortableAdminMixin, admin.ModelAdmin):
+  pass
+
+admin.site.register(Channels, ChannelsAdmin)
 admin.site.register(Epg)
 # Register your models here.
