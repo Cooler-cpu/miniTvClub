7767d2046ce3f56f7551fc8aff27299f2bdaa1a3
vladislav babichev <babichev_vladislav@list.ru>
2021-07-04 22:24:59 +0300

    playlist, token serializers

--- web/api/playlists/serializers.py
+++ web/api/playlists/serializers.py
@@ -1,21 +1,26 @@
 from rest_framework import serializers
 
-from .models import Playlists
+from .models import Playlists, Ips
+
+from users.serializers import UserSerializers
+from channels.serializers import PacketSerializer
+from tokens.serializer import TokenSerializer
+
+class IpsSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model = Ips
+        exclude = ("id",)
 
 
-# class PacketSerializers(serializers.Serializer):
-#     packet = serializers.SlugRelatedField()
+class PlaylistSerializers(serializers.ModelSerializer):
+    user = UserSerializers()
+    packets = PacketSerializer(many=True)
+    token = TokenSerializer()
+    allowaddip = IpsSerializers(many=True)
 
 
+    class Meta:
+        model = Playlists
+        exclude = ("id",)
 
-class PlaylistSerializers(serializers.Serializer):
-    pass
-    # name = serializers.CharField(max_length=50)
-    # user = serializers.IntegerField()
-    # type_autopay = serializers.CharField(max_length=1)
-    # type_pay = serializers.CharField(max_length=1)
-    # status = serializers.CharField(max_length=1)
-    # # packets = PacketSerializers(many=True)
-    # data_start = serializers.DateTimeField()
-    # data_stop = serializers.DateTimeField()
-    # token = serializers.IntegerField()
