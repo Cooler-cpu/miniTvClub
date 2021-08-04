ecf0e2c118da42275300fd4e6928432c6564dfa8
Maxim Shevtsov <68514853+Salivanch@users.noreply.github.com>
2021-08-04 21:58:03 +0300

    Update urls.py

--- web/api/django_app/urls.py
+++ web/api/django_app/urls.py
@@ -9,18 +9,12 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     url('server', include(('fluss_servers.urls', 'fluss_servers'), namespace='fluss_servers')),
-
-    
     path('grappelli/', include('grappelli.urls')),
     path('_nested_admin/', include('nested_admin.urls')),
     path('admin/', admin.site.urls),
     path('test', Load.as_view()),
-<<<<<<< HEAD
-
-=======
     path('api/', include('channels.urls')),
     path('', include('fluss_streams.urls')),
->>>>>>> 42fea2242f6173ae3a55d2f88e344deab41ea784
 ]
 
 if settings.DEBUG:
