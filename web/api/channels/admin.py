87a728792b40834c41f70f47d5ab6774bf27b492
Maxim Shevtsov <68514853+Salivanch@users.noreply.github.com>
2021-08-21 14:18:01 +0300

    preview img channels

--- web/api/channels/admin.py
+++ web/api/channels/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Channels, Epg, Packets
 
 from adminsortable2.admin import SortableAdminMixin
@@ -6,6 +7,10 @@ from adminsortable2.admin import SortableAdminMixin
 
 class ChannelsAdmin(SortableAdminMixin, admin.ModelAdmin):
   list_display = ("id","name", "stream")
+  readonly_fields = ('preview',)
+  
+  def preview(self, obj):
+    return format_html('<img src="{}" width="{}" height="{}" style="object-fit: contain;">'.format(obj.logo.url, 150, 150))
 
 
 class EpgAdmin(SortableAdminMixin, admin.ModelAdmin):
